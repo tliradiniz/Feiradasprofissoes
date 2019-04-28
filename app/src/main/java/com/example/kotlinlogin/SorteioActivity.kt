362a97c59157a77ef41201809f670c81139b9468
tliradiniz <tdiniz22@gmail.com>
2019-04-28 20:35:01 -0300

    Sorteio update

--- app/src/main/java/com/example/kotlinlogin/SorteioActivity.kt
+++ app/src/main/java/com/example/kotlinlogin/SorteioActivity.kt
@@ -2,10 +2,25 @@ package com.example.kotlinlogin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+
 
 class SorteioActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sorteio)
+    }
+
+    fun goToIG(view: View) {
+        goToUrl("https://www.instagram.com")
+    }
+
+
+    private fun goToUrl(url: String) {
+        val uriUrl = Uri.parse(url)
+        val launchBrowser = Intent(Intent.ACTION_VIEW, uriUrl)
+        startActivity(launchBrowser)
     }
 }
